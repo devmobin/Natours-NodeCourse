76219777c018dddc549f29d311657e900a7a6da0
Mobin Heydari <mobinheydari114@gmail.com>
2020-04-16 23:44:32 +0430

    config server

---
+++ V2/server.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', (error) => {
+  console.log('UNCAUGHT EXCEPTION!  Shutting down...');
+  console.log(error.name, error.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const MongoURI = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(MongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DataBase connection successful!'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
+
+process.on('unhandledRejection', (error) => {
+  console.log('UNHANDLED REJECTION!  Shutting down...');
+  console.log(error.name, error.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('Process terminated!');
+  });
+});
